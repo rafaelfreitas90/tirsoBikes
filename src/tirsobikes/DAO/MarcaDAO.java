72f724b42919e4b7d792f95cc4d2d8d0727ce7e8
luishenrique <luishenrik3@gmail.com>
2013-10-15 15:20:26 -0300

    Dao Marca

---
+++ src/tirsobikes/DAO/MarcaDAO.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tirsobikes.DAO;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import tirsobikes.entidades.Marca;
+
+/**
+ *
+ * @author LuisHenrique
+ */
+public class MarcaDAO {
+    private EntityManager manager;
+
+    public MarcaDAO() {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("TirsoBikesPU");
+        manager = factory.createEntityManager();
+    }
+
+    public Marca salvarMarca(Marca marca) {
+        manager.getTransaction().begin();
+        manager.persist(marca);
+        manager.getTransaction().commit();
+        return marca;
+    }
+
+    public void atualizarMarca(Marca marca) {
+        manager.getTransaction().begin();
+        manager.merge(marca);
+        manager.getTransaction().commit();
+    }
+
+    public void deletarMarca(Marca marca) {
+        manager.getTransaction().begin();
+        manager.remove(marca);
+        manager.getTransaction().commit();
+    }
+
+    public Marca procurarMarca(int id) {
+        return manager.find(Marca.class, id);
+    }
+
+    public List<Marca> ListaMarcas() {
+        Query query = manager.createQuery("from Marca");
+        return query.getResultList();
+    }
+}
