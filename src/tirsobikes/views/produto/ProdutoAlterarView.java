3f574461d7bcb6c02254ecef0a4c49457402a3d0
Rafael Freitas <rsfreitas90@gmail.com>
2013-11-01 11:01:52 -0200

    up

--- src/tirsobikes/views/produto/ProdutoAlterarView.java
+++ src/tirsobikes/views/produto/ProdutoAlterarView.java
@@ -35,7 +35,7 @@ public class ProdutoAlterarView extends javax.swing.JFrame {
         bntAlterar = new javax.swing.JButton();
         bntAlterar1 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Lista de Produtos");
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowActivated(java.awt.event.WindowEvent evt) {
